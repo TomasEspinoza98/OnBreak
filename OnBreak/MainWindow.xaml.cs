37fc1fc3a683dcd3667ab7ca877d0bce24d05c30
tomasespinoza98 <tom.espinozah@alumnos.duoc.cl>
2020-05-11 21:29:38 -0400

    first commit

---
+++ OnBreak/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MahApps.Metro.Controls;
+using OnBreak.vistas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OnBreak {
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            AdminCliente window = new AdminCliente();
+            window.Show();
+        }
+    }
+}
---
+++ OnBreak/vistas/AdminCliente.xaml.cs
@@ -0,0 +1,25 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OnBreak.vistas {
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class AdminCliente : MetroWindow {
+        public AdminCliente() {
+            InitializeComponent();
+        }
+    }
+}
